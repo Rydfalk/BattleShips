7b2ba407fa2c17b2fa9f7793b3831bba3ce9e0af
Rydfalk <v.rydfalk@gmail.com>
2013-12-13 14:00:16 +0100

    Made the main JFrame. Still testing git.

--- BattleShips/src/main/Main.java
+++ BattleShips/src/main/Main.java
@@ -1,8 +1,19 @@
 package main;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		JFrame mainFrame = new JFrame("Battleships");
+		
+		mainFrame.setSize(800, 600);
+		mainFrame.setLocationRelativeTo(null);
+		mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		mainFrame.setVisible(true);
+		
 		
 	}
 
