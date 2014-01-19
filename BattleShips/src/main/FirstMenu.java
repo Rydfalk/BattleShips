1f21352017d05bdc2a936022a47c59139b389717
Rydfalk <v.rydfalk@gmail.com>
2014-01-19 09:53:42 +0100

    and now I have committed it aswell

---
+++ BattleShips/src/main/FirstMenu.java
@@ -0,0 +1,56 @@
+package main;
+
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+
+class FirstMenu extends JPanel {
+	private JButton statisticsButton;
+	private JButton gameButton;
+	private JPanel contentPane;
+/*************************** Constructors ******************************/
+	
+/*************************** Public Methods ******************************/
+	public FirstMenu(JPanel panel) {
+		contentPane = panel;
+		setOpaque(true);
+		setBackground(Color.RED.darker().darker());
+		
+		statisticsButton = new JButton("Statistics");
+		statisticsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				CardLayout cardLayout = (CardLayout) contentPane.getLayout();
+				cardLayout.show(contentPane, "Statistics Card" );
+				
+			}
+		});
+		add(statisticsButton);
+		
+		
+		gameButton = new JButton("Battleships");
+		gameButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				CardLayout cardLayout = (CardLayout) contentPane.getLayout();
+				cardLayout.show(contentPane, "Game Card" );
+				
+			}
+		});
+		add(gameButton);
+	}
+	
+	
+
+	@Override
+	public Dimension getPreferredSize() {
+		return (new Dimension(500, 500));
+	}
+}
